5deb5e9d98bd63b14a47ebd3f4e3a3f7541a94e7
McDodgerman <59996045+McDodgerman@users.noreply.github.com>
2021-07-14 10:55:37 -0500

    update
    Course completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,90 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.deepPurple[400],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/iconBLK.jpg'),
+              ),
+              Text(
+                'Kinsey Luft',
+                style: TextStyle(
+                  fontSize: 60.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Trigot',
+                  letterSpacing: 1.8,
+                ),
+              ),
+              Text(
+                'STARGRAZER',
+                style: TextStyle(
+                  color: Colors.deepPurple[100],
+                  fontSize: 24.0,
+                  fontFamily: 'Permanent Marker',
+                  fontStyle: FontStyle.italic,
+                  letterSpacing: 4.0,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.deepPurple[100],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 12.0, horizontal: 36.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.tealAccent[700],
+                  ),
+                  title: Text(
+                    '123 456 7890',
+                    style: TextStyle(
+                      color: Colors.deepPurple[400],
+                      fontSize: 24.0,
+                      fontFamily: 'Trigot',
+                      letterSpacing: 2.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 12.0, horizontal: 36.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.tealAccent[700],
+                  ),
+                  title: Text(
+                    'LxNguyen017@gmail.com',
+                    style: TextStyle(
+                      color: Colors.deepPurple[400],
+                      fontSize: 24.0,
+                      fontFamily: 'Trigot',
+                      letterSpacing: 1.2,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
